1f18c83e82c70dc8accf7ebccd10b7b4e5b0e79b
tejash raju <tejashraju@tejashs-Air.lan>
2023-06-13 23:57:36 -0400

    Autopush

---
+++ vars/python.groovy
@@ -0,0 +1,24 @@
+def lintchecks(COMPONENT) {
+    sh''' 
+        echo lint checks for ${COMPONENT}
+        # pylint *.py
+        echo PERFORMING LINT CHECKS for ${COMPONENT}  
+        echo PERFORMING LINT CHECKS COMPLETED for ${COMPONENT}  
+    '''                
+}
+
+// Call is the default function which will be called when you call the fileName
+def call() {
+    pipeline {
+        agent any
+            stages {
+            stage('Lint checks') {
+                steps {
+                    script {
+                        lintchecks()
+                    }
+                }
+            }
+        }
+    }
+}
