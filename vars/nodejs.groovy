c6c8418071db37ccbef21fd766bcd91a70dafba0
tejash raju <tejashraju@tejashs-Air.lan>
2023-07-13 17:33:44 -0400

    Autopush

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -30,6 +30,7 @@ def call() {
             stage('Sonar checks') {
                 steps {
                     script {
+                        env.ARGS = "-Dsonar.sources=."
                         common.sonarChecks()
                     }
                 }
