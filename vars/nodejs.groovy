29fcea5efa3658bc4178e2709217c32c939418d2
tejash raju <tejashraju@tejashs-Air.lan>
2023-07-13 16:21:25 -0400

    Autopush

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -13,6 +13,8 @@ def lintchecks(COMPONENT) {
 def sonarChecks() {
     sh '''
         sonar-scanner -Dsonar.host.url=http://172.31.84.244:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}
+        curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh
+        bash -x quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}
     '''
 }
 
@@ -22,9 +24,9 @@ def call() {
         agent any
         environment { 
         SONAR = credentials('SONAR')
+        SONAR_URL = "172.31.84.244"
     }
-            stages {
-            
+        stages {
             stage('Lint checks') {
                 steps {
                     script {
